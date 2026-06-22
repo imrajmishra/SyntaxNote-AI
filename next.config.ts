17bdbdeac37df7caca1587de82b7ee73fd0f98e2
Raj Mishra <raj212558@gmail.com>
2026-06-22 07:20:19 +0530

    Set Next.js output to 'export' for static export

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export',  // Tells Next.js to generate the /out folder
 
   images: {
     remotePatterns: [
